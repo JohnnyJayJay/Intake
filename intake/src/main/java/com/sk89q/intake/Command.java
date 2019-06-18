ce7fa47642be069332ebc818563659dd06088769
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-06-18 15:54:06 +0200

    Remove legacy @Command properties

--- intake/src/main/java/com/sk89q/intake/Command.java
+++ intake/src/main/java/com/sk89q/intake/Command.java
@@ -60,20 +60,6 @@ public @interface Command {
   String desc();
 
   /**
-   * The minimum number of arguments. This should be 0 or above.
-   *
-   * @return The minimum number of arguments
-   */
-  int min() default 0;
-
-  /**
-   * The maximum number of arguments. Use -1 for an unlimited number of arguments.
-   *
-   * @return The maximum number of arguments
-   */
-  int max() default -1;
-
-  /**
    * Flags allow special processing for flags such as -h in the command, allowing users to easily
    * turn on a flag. This is a string with each character being a flag. Use A-Z and a-z as possible
    * flags. Appending a flag with a : makes the flag character before a value flag, meaning that if
